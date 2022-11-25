9c433eb336fac0134592fa86e07b80bfee9ce7f8
ojpe145 <osnaider_pacheco14@hotmail.com>
2022-11-24 23:34:36 -0500

    second commit

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -8,7 +8,7 @@ const Inicio = () => {
             <ul type="square">
               <h1 className="text-center mt-3 mb-4" >Rubrica Web</h1>
               <center>
-              <p>Creado por: Ivan Lafaurie, Ithamar Lara</p>
+              
               <img class="img-fluid" src="https://cdn.discordapp.com/attachments/949443541334118541/1045381927743537263/unknown.png" alt="logo"></img>
               </center>
             </ul>
